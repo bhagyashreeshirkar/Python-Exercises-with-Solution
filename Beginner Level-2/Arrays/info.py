d759e331786df11780e23b765b52d681ee038f3c
Bhagyashree Shirkar <67758900+bhagyashreeshirkar@users.noreply.github.com>
2021-05-02 01:12:50 +0530

    Add files via upload

---
+++ Beginner Level-2/Arrays/info.py
@@ -0,0 +1,19 @@
+"""
+1. array is beneficial if you need to store group of elements of same datatype.
+for e.g [2, 12, 33, 45, 78, 90]
+        ['a', 'b', 'c', 'd']
+        [2.1, 4.5, 2.9, 7.3, 9.6]
+
+2. arrays and list in python are not same; but array modules rarely used in python.
+
+3. The size of an array is not fixed, it can increase or decrease their size dynamically.
+
+4. arrays are small in size as compared to lists.
+
+5. for data science we use numpy arrays for calculations but not normal arrays modules.
+
+6.Arrays need to be declared whereas lists do not need declaration because they are a part of Python's syntax.
+    (This is the reason lists are more often used than arrays.)
+But in case you want to perform some arithmetic function to your list, one should go with arrays instead. ie numpy array
+
+"""
